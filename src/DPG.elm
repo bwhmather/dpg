dcf530b63419e1fc615676af0d96a2eb46526989
Ben Mather <bwhmather@bwhmather.com>
2015-02-04 13:39:14 +0000

    more refactoring

--- src/DPG.elm
+++ src/DPG.elm
@@ -4,32 +4,40 @@ import Maybe
 import Signal
 import Signal (Signal, Channel, Message)
 
-import Html (Html)
+import Html (Html, div)
 
 import DPG.Utils (filterMap)
 import DPG.Target as Target
 
+type alias Model =
+    { target : Target.Model
+    }
 
 type Action
     = TargetAction Target.Action
     | NoOp
 
-sendTargetAction : Channel Action -> Target.Action -> Message
-sendTargetAction updates action = Signal.send updates (TargetAction action)
-
-targetActions : Signal Action -> Signal Target.Action
-targetActions = filterMap
-    (\ action -> case action of
-        TargetAction tAction -> Just tAction
-        _ -> Nothing)
-    Target.NoOp
-
-
 updates : Channel Action
 updates = Signal.channel NoOp
 
-model : Signal Target.Model
-model = Signal.foldp Target.update Target.emptyModel (targetActions (Signal.subscribe updates))
+emptyModel : Model
+emptyModel =
+    { target = Target.emptyModel
+    }
+
+update : Action -> Model -> Model
+update action model = case action of
+    TargetAction a -> { model | target <- Target.update a model.target }
+    _ -> model
+
+model : Signal Model
+model = Signal.foldp update emptyModel (Signal.subscribe updates)
+
+view : (Action -> Message) -> Model -> Html
+view send model =
+    div [] [
+        Target.view (\m -> send (TargetAction m)) model.target
+    ]
 
 main : Signal Html
-main = Signal.map (Target.view (sendTargetAction updates)) model
+main = Signal.map (view (Signal.send updates)) model
