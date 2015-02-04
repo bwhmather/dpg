0434eed7925b83146b4c473791da1dfc5753c6f9
Ben Mather <bwhmather@bwhmather.com>
2015-02-04 22:48:05 +0000

    basic plumbing of noise source

--- src/Dpg.elm
+++ src/Dpg.elm
@@ -2,9 +2,11 @@ module Dpg where
 
 import Maybe
 import Result
+import Result (Result (Ok, Err))
 import Signal
 import Signal (Signal, Channel, Message)
 
+import Html
 import Html (Html, div)
 
 import Dpg.Target as Target
@@ -36,10 +38,13 @@ update action model = case action of
         { model | generator <- Generator.update a model.generator }
     _ -> model
 
-view : (Action -> Message) -> Model -> Html
-view send model =
+view : (Action -> Message) -> Model -> Result String String -> Html
+view send model output =
     div []
     [ Target.view (\m -> send (TargetAction m)) model.target
+    , case output of
+        Ok password -> Html.text ("password: " ++ password)
+        Err message -> Html.text ("error: " ++ message)
     , Generator.view (\m -> send (GeneratorAction m)) model.generator
     ]
 
@@ -52,5 +57,15 @@ model = Signal.foldp update emptyModel (Signal.subscribe updates)
 noiseSource : Source.NoiseSource
 noiseSource = Source.new (Signal.map (\m -> Result.toMaybe (Target.output m.target)) model)
 
+
+updateOutput : Model -> Source.Output -> Result String String
+updateOutput model noiseOutput = case noiseOutput of
+    Source.Ok noise -> Generator.output model.generator noise
+    _ -> Err "TODO"
+
+output : Signal (Result String String)
+output = Signal.map2 updateOutput model (Source.status noiseSource)
+
+
 main : Signal Html
-main = Signal.map (view (Signal.send updates)) model
+main = Signal.map2 (view (Signal.send updates)) model output
