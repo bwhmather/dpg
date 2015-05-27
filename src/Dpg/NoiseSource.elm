c4279dff2d45cad9d60c467823d4dcab664061b1
Ben Mather <bwhmather@bwhmather.com>
2015-05-27 21:14:09 +0100

    start of webworker based noise source

--- src/Dpg/NoiseSource.elm
+++ src/Dpg/NoiseSource.elm
@@ -1,33 +1,137 @@
-module Dpg.NoiseSource where
+module Dpg.NoiseSource
+    ( Request(Request, Nothing)
+    , Output(NoResult, Progress, Result, Error)
+    , Noise
+    ) where
+
+import Json.Encode as JE
 
 import String exposing (toUpper)
 import Signal
 import Time exposing (delay, second)
 
+import WebWorker
+
 type alias Noise = String
 type NoiseSource = NoiseSource (Signal Output)
 
 
+type Request
+  = Request
+      { password : String
+      , salt : String
+      , bytes : Int
+      }
+  | Nothing
+
+
+{-| Update to internal state
+|-}
+type Update
+    = RequestStart { password : String, salt : String, bytes : Int }
+    | RequestStop
+    | NotifyAccepted
+    | NotifyProgress Float
+    | NotifyCompleted Noise
+    | NotifyError String
+    | NoOp
+
+
+{-| Internal state
+|-}
+type State
+    = Waiting Int
+    | Stopping Int
+    | InProgress Float
+    | Completed Noise
+    | Errored String
+
+
+{-| Public state
+|-}
 type Output
-    = Waiting
+    = NoResult
     | Progress Float
-    | Ok Noise
-    | Err String
+    | Result Noise
+    | Error String
 
-makeOutput : Maybe String -> Output
-makeOutput request = case request of
-    Just string -> Ok (toUpper string)
-    Nothing -> Waiting
+requestToMessage : Request -> WebWorker.Request
+requestToMessage req = case req of
+    Request req -> WebWorker.SendMessage <| JE.object
+        [ ("password", JE.string req.password)
+        , ("salt", JE.string req.salt)
+        , ("bytes", JE.int req.bytes)
+        ]
+    Nothing -> WebWorker.NoOp
 
-new : Signal (Maybe String) -> NoiseSource
-new requests = NoiseSource (Signal.map makeOutput (delay (2 * second) requests))
 
-status : NoiseSource -> Signal Output
-status (NoiseSource stream) = stream
+requestToStateUpdate : Request -> Update
+requestToStateUpdate req = case req of
+    Request req -> RequestStart req
+    Nothing -> RequestStop
 
-noise : NoiseSource -> Signal (Maybe Noise)
-noise (NoiseSource stream) = Signal.map
-    (\o -> case o of
-        Ok noise -> Just noise
-        _ -> Nothing)
-    stream
+
+messageToStateUpdate : WebWorker.Response -> Update
+messageToStateUpdate msg = case msg of
+    WebWorker.Waiting -> NoOp
+    WebWorker.Message msg -> NotifyAccepted  -- TODO
+    WebWorker.Error msg -> NotifyError msg
+
+
+applyStateUpdate : Update -> State -> State
+applyStateUpdate update state = case update of
+    RequestStart params -> case state of
+        Waiting count -> Waiting (count + 1)
+        Stopping count -> Waiting (count + 1)
+        Errored msg -> Errored msg
+        _ -> Waiting 0
+    RequestStop -> case state of
+        Waiting count -> Stopping count
+        Stopping count -> Stopping count
+        InProgress _ -> Stopping 0
+        Completed _ -> Stopping 0
+        Errored msg -> Errored msg
+    NotifyAccepted -> case state of
+        Waiting 0 -> InProgress 0.0
+        Waiting n -> Waiting (n - 1)
+        Stopping n -> Stopping (n - 1)
+        InProgress _ -> Errored "worker restarted unexpectedly"
+        Completed _ -> Errored "worker restarted unexpectedly"
+        Errored msg -> Errored msg
+    NotifyProgress progress -> case state of
+        Waiting _ -> state
+        Stopping _ -> state
+        InProgress _ -> InProgress progress
+        Completed _ -> Errored "unexpected progress update"
+        Errored msg -> Errored msg
+    NotifyCompleted res -> case state of
+        Waiting _ -> state
+        Stopping _ -> state
+        InProgress _ -> Completed res
+        Completed _ -> Errored "worker already finished"
+        Errored msg -> Errored msg 
+    NotifyError msg -> case state of
+        Errored _ -> state
+        _ -> Errored msg
+    NoOp -> state
+
+
+renderState : State -> Output
+renderState state = case state of
+    Stopping _ -> NoResult
+    Waiting _ -> Progress 0.0
+    InProgress progress -> Progress progress
+    Completed res -> Result res
+    Errored msg -> Error msg
+
+
+new : Signal Request -> Signal Output
+new requests =
+    let messages = WebWorker.spawn "worker" (Signal.map requestToMessage requests)
+    in
+        (Signal.map renderState
+            (Signal.foldp applyStateUpdate (Stopping 0) (Signal.merge
+                (Signal.map requestToStateUpdate requests)
+                (Signal.map messageToStateUpdate messages))))
+
+
---
+++ src/Native/WebWorker.js
@@ -0,0 +1,49 @@
+// TODO belongs in bwhmather/elm-webworker package
+
+Elm.Native.WebWorker = {};
+Elm.Native.WebWorker.make = function(elm) {
+    elm.Native = elm.Native || {};
+    elm.Native.WebWorker = elm.Native.WebWorker || {};
+    if (elm.Native.WebWorker.values) {
+        return elm.Native.WebWorker.values;
+    }
+
+    var Signal = Elm.Signal.make(elm);
+    var NS = Elm.Native.Signal.make(elm);
+    var List = Elm.Native.List.make(elm);
+
+    function spawn(url, requests) {
+        var responses = NS.input({ctor: 'Waiting'});
+
+        if (!window.Worker) {
+            elm.notify(responses.id, {ctor: 'Error', _0: "webworkers not supported"});
+            return responses;
+        }
+
+        var w = new Worker(url);
+
+        w.onmessage = function(event) {
+            elm.notify(responses.id, {ctor: 'Message', _0: event.data});
+        };
+
+        w.onerror = function(event) {
+            elm.notify(responses.id, {ctor: 'Error', _0: event.message});
+        };
+
+        function processRequest(req) {
+            if (req.ctor === 'SendMessage') {
+                w.postMessage(req._0);
+            }
+        };
+
+        function take1(x,y) { return x }
+
+        return A3(
+            Signal.map2, F2(take1),
+            responses,
+            A2(Signal.map, processRequest, requests)
+        );
+    }
+    return elm.Native.WebWorker.values = { spawn: F2(spawn) };
+};
+
