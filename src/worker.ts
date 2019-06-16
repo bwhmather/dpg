076f2e0be37dd628ed208cf7634266568f1afd32
Ben Mather <bwhmather@bwhmather.com>
2019-06-16 10:17:18 +0100

    Fix tslint warnings in worker script

--- src/worker.ts
+++ src/worker.ts
@@ -1,19 +1,19 @@
 import { hashBytes } from "./skein";
 
-
 const CHARACTERS = {
   LOWER_CASE: "abcdefghijklmnopqrstuvwxyz",
-  UPPER_CASE: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   NUMERIC: "0123456789",
   SPECIAL: "!\"#$%&'()*+,-./:;<=>>@[\\]^_`{|}~",
+  UPPER_CASE: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
 };
 
 function generatePassword(seed, settings) {
+  // tslint:disable:no-bitwise
   let i;
   let password = "";
 
   // Create a string of characters from which to build the password.
-  let source_characters = (
+  const sourceCharacters = (
     (settings.enableLowercase ? CHARACTERS.LOWER_CASE : "") +
     (settings.enableUppercase ? CHARACTERS.UPPER_CASE : "") +
     (settings.enableNumbers ? CHARACTERS.NUMERIC : "") +
@@ -21,7 +21,7 @@ function generatePassword(seed, settings) {
   );
 
   // Build the password character by character.
-  for (i=0; i < settings.outputLength; i++) {
+  for (i = 0; i < settings.outputLength; i++) {
     // Generate a 24 bit random number from from first three bytes of seed */
     // Because of the way bitwise operators are handled in javascript, larger
     // numbers will cause issues.
@@ -36,23 +36,23 @@ function generatePassword(seed, settings) {
     num += settings.outputLength;
 
     // Select character from those available.
-    password += source_characters[num % source_characters.length];
+    password += sourceCharacters[num % sourceCharacters.length];
 
     // Update seed using lagged fibonacci generator.  See Wikipedia.  Values
     // borrowed from Knuth.  Using a 512 byte hash this step is really
     // unnecessary for passwords less than 64 characters long.
-    seed.unshift(seed[24-1] ^ seed[55-1]);
+    seed.unshift(seed[24 - 1] ^ seed[55 - 1]);
     seed.pop();
   }
 
   return password;
-};
-
+  // tslint:enable:no-bitwise
+}
 
 function generate(details, settings) {
   // Build the string from which to obtain the hash used to generate the
   // password.
-  let input = (
+  const input = (
     details.hostname +
     details.username +
     details.password +
@@ -60,19 +60,19 @@ function generate(details, settings) {
   );
 
   // Hash the string to produce a seed for the password generator.
-  var hash = hashBytes(new TextEncoder().encode(input));
+  const hash = hashBytes(new TextEncoder().encode(input));
 
   // Generate password from seed.
-  var password = generatePassword(hash, settings);
+  const password = generatePassword(hash, settings);
 
   return password;
 }
 
 function onmessage(msg) {
-  let password = generate(msg.data.details, msg.data.settings);
+  const password = generate(msg.data.details, msg.data.settings);
   postMessage(password);
 }
 
-
-const postMessage = <(any) => void> self.postMessage;
+// tslint:disable-next-line:variable-name
+const postMessage = self.postMessage as (any) => void;
 self.onmessage = onmessage;
